6b7280c6ea6258602b6d466e596cf100fafa1b2c
Malte <mama-lolo@web.de>
2020-06-06 21:34:19 +0200

    implemented Player as a class

--- game.py
+++ game.py
@@ -1,10 +1,27 @@
 import pygame
+
+
 def main():
-    #Setup
+    # Data structures
+    player = Player(0, 0, 0)
+    # Setup
     pygame.init()
+    print(player)
 
 
+class Player():
+    def __init__(self, pos_x, pos_y, velocity=0.0):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.velocity = velocity
 
+    def __str__(self):
+        output = "{ "
+        output += "x:"+str(self.pos_x)+" || "
+        output += "y:"+str(self.pos_y)+" || "
+        output += "velocity:"+str(self.velocity)+" || "
+        output += "}"
+        return output
 
 
 if __name__ == "__main__":
